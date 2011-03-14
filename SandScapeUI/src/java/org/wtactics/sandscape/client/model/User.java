498005da8c57ce802b4afcf66f25d903cfd3dde1
Sérgio Lopes <sergiolopes@aluminumboard.local>
2011-03-14 00:54:52 +0000

    Removed SandScapeUI project that was using GWT.

--- SandScapeUI/src/java/org/wtactics/sandscape/client/model/User.java
+++
@@ -1,9 +0,0 @@
-package org.wtactics.sandscape.client.model;
-
-import com.google.gwt.core.client.JavaScriptObject;
-
-public class User extends JavaScriptObject {
-
-    protected User() {
-    }
-}
