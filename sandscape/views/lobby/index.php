dc6c722415fccfd0a1e97fa86142bfe6f9999076
Sérgio Lopes <sergiolopes@aluminumboard.local>
2011-03-28 22:07:23 +0100

    minor update to lobby page and css

--- sandscape/views/lobby/index.php
+++ sandscape/views/lobby/index.php
@@ -2,23 +2,26 @@
     <div id="lobby-left">
         <div id="lobby-users">
             <ul>
-                <li>User 1</li>
-                <li>User 2</li>
-                <li>User 3</li>
+                <li><a href="#">User 1</a></li>
+                <li><a href="#">User 2</a></li>
+                <li><a href="#">User 3</a></li>
             </ul>
         </div>
         <div id="lobby-chat">chat</div>
         <div class="clear"></div>
         <div id="lobby-tools">
             <form action="#" method="post">
-                <input type="text" id="message" name="message"  />
-                <button type="submit">OK</button>
+                <input type="text" id="message" name="message" size="90" />
+                <button type="button">Send</button>
             </form>
         </div>
     </div>
     <div id="lobby-right">
         <ul>
-            <li>Game 1</li>
+            <li><a href="#">Game 1</a></li>
+            <li><a href="#">Game 2</a></li>
+            <li><a href="#">Game 3</a></li>
+            <li><a href="#">Game 4</a></li>
         </ul>
     </div>
     <div class="clear"></div>
