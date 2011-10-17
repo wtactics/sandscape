9d2793df2a7f62c73ffe0a756d1fbebaebafb080
Sérgio Lopes <knitter.is@gmail.com>
2011-10-18 00:16:47 +0100

    Corrected menu to display on hover, still missing correct styles.

--- sandscape/views/layouts/site.php
+++ sandscape/views/layouts/site.php
@@ -1,16 +1,26 @@
+<?php
+Yii::app()->clientScript->registerScript('menuhover', "
+        $('#menu > ul li').hover(function() {
+            $(this).addClass('hover');
+            $('ul:first', this).css('visibility', 'visible');
+        }, function() {
+            $(this).removeClass('hover');
+            $('ul:first',this).css('visibility', 'hidden');
+        });");
+?>
 <!DOCTYPE HTML>
 <html>
     <head>
         <meta charset="UTF-8" />
         <!-- blueprint css -->
-        <link href="_resources/css/blueprint/screen.css" rel="stylesheet" type="text/css" media="screen,projection" />
+        <link href="_resources/css/blueprint/screen.css" rel="stylesheet" type="text/css" media="screen, projection" />
         <link href="_resources/css/blueprint/print.css" rel="stylesheet" type="text/css" media="print" />
         <!--[if lt IE 8]>
-        <link href="_resources/css/blueprint/ie.css" rel="stylesheet" type="text/css" media="screen,projection" />
+        <link href="_resources/css/blueprint/ie.css" rel="stylesheet" type="text/css" media="screen, projection" />
         <![endif]-->
 
         <!-- css -->
-        <link href="_resources/css/main.css" rel="stylesheet" type="text/css" media="screen,projection" />
+        <link href="_resources/css/main.css" rel="stylesheet" type="text/css" media="screen, projection" />
 
         <title></title>
     </head>
