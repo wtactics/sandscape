28cfb03f4e8b858c624682454edb49e78af44870
Sérgio Lopes <knitter.is@gmail.com>
2012-03-01 11:45:50 +0000

    Correcting SQL errors that were, somehow, kept in the source files (thanks to Simon McGregor for finding them).

--- _dev/database/v1.0/20111020.sql
+++ _dev/database/v1.0/20111020.sql
@@ -8,14 +8,14 @@ ALTER TABLE `Game` DROP `deck1`;
 ALTER TABLE `Game` DROP `deck2`;
 
 -- add max number of decks per player
-ALTER TABLE `Game` ADD `maxDecks` TNINYINT NOT NULL DEFAULT 1;
+ALTER TABLE `Game` ADD `maxDecks` TINYINT NOT NULL DEFAULT 1;
 
 -- the game has a graveyard?
-ALTER TABLE `Game` ADD `graveyard` TNINYINT NOT NULL DEFAULT 1;
+ALTER TABLE `Game` ADD `graveyard` TINYINT NOT NULL DEFAULT 1;
 
 -- players are ready
-ALTER TABLE `Game` ADD `player1Ready` TNINYINT NOT NULL DEFAULT 0,
-ADD `player2Ready` TNINYINT NOT NULL DEFAULT 0;
+ALTER TABLE `Game` ADD `player1Ready` TINYINT NOT NULL DEFAULT 0,
+ADD `player2Ready` TINYINT NOT NULL DEFAULT 0;
 
 -- CREATE Game / Deck link
 CREATE TABLE GameDeck (
@@ -31,8 +31,8 @@ CREATE TABLE `DeckCard` (
 `dcId` INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 `deckId` INT UNSIGNED NOT NULL,
 `cardId` INT UNSIGNED NOT NULL,
-`CONSTRAINT `fkDCDeck` FOREIGN KEY (`deckId`) REFERENCES `Deck`(`deckId`),
-`CONSTRAINT `fkDCCard` FOREIGN KEY (`cardId`) REFERENCES `Card`(`cardId`)
+CONSTRAINT `fkDCDeck` FOREIGN KEY (`deckId`) REFERENCES `Deck`(`deckId`),
+CONSTRAINT `fkDCCard` FOREIGN KEY (`cardId`) REFERENCES `Card`(`cardId`)
 ) ENGINE=InnoDB character set utf8 collate utf8_unicode_ci;
 
 ALTER TABLE `ChatMessage` ADD `system` TINYINT NOT NULL DEFAULT 0 ;
