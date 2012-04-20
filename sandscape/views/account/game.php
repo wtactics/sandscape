a42d6d197031ef0c9c392f362ee7c1208f8cd86b
Sérgio Lopes <knitter.is@gmail.com>
2012-04-20 22:46:16 +0100

    Updating minified versions of the CSS files

--- sandscape/views/account/game.php
+++ sandscape/views/account/game.php
@@ -66,27 +66,29 @@
     </tr>
 </table>
 
-<h3>Own Decks</h3>
-<table>
-    <?php
-    $url = $this->createUrl('account/starratingajax');
-    $url2 = $this->createUrl('account/notesajax');
 
-    foreach ($decks as $entry) {
-        $deck = $entry['deck'];
-        $stats = $entry['stats'];
-        ?>
-        <tr>
-            <td><?php echo CHtml::link($deck->name, $this->createUrl('decks/view', array('id' => $deck->deckId))); ?></td>
-            <td><?php echo count($deck->deckCards); ?> cards</td>
-            <td>
-                <?php
-                $this->widget('CStarRating', array(
-                    'name' => 'rating',
-                    'minRating' => 1,
-                    'maxRating' => 7,
-                    'starCount' => 7,
-                    'callback' => "function() {
+<?php if ($game->ended) { ?>
+    <h3>Own Decks</h3>
+    <table>
+        <?php
+        $url = $this->createUrl('account/starratingajax');
+        $url2 = $this->createUrl('account/notesajax');
+
+        foreach ($decks as $entry) {
+            $deck = $entry['deck'];
+            $stats = $entry['stats'];
+            ?>
+            <tr>
+                <td><?php echo CHtml::link($deck->name, $this->createUrl('decks/view', array('id' => $deck->deckId))); ?></td>
+                <td><?php echo count($deck->deckCards); ?> cards</td>
+                <td>
+                    <?php
+                    $this->widget('CStarRating', array(
+                        'name' => 'rating',
+                        'minRating' => 1,
+                        'maxRating' => 7,
+                        'starCount' => 7,
+                        'callback' => "function() {
                             $.ajax({
                                 type: 'POST',
                                 url: '{$url}',
@@ -97,32 +99,32 @@
                                 }
                             })
                         }"
-                ));
-                ?>
-            </td>
-            <td>
-                <?php
-                echo CHtml::link(
-                        CHtml::image('_resources/images/icon-x16-document-pencil.png', '', array('title' => 'Edit notes'))
-                        , 'javascript:;', array(
-                    'onclick' => "$('#dlgGameId').val({$game->gameId});$('#dlgDeckId').val({$deck->deckId});$('#notesDlg').dialog('open');"
-                ));
-                ?>
-            </td>
-        </tr>
-    <?php } ?>
-</table>
+                    ));
+                    ?>
+                </td>
+                <td>
+                    <?php
+                    echo CHtml::link(
+                            CHtml::image('_resources/images/icon-x16-document-pencil.png', '', array('title' => 'Edit notes'))
+                            , 'javascript:;', array(
+                        'onclick' => "$('#dlgGameId').val({$game->gameId});$('#dlgDeckId').val({$deck->deckId});$('#notesDlg').dialog('open');"
+                    ));
+                    ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
 
-<?php
-$this->beginWidget('zii.widgets.jui.CJuiDialog', array(
-    'id' => 'notesDlg',
-    'options' => array(
-        'title' => 'Notes',
-        'autoOpen' => false,
-        'height' => 390,
-        'width' => 440,
-        'buttons' => array(
-            'Save' => "js:function() {
+    <?php
+    $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
+        'id' => 'notesDlg',
+        'options' => array(
+            'title' => 'Notes',
+            'autoOpen' => false,
+            'height' => 390,
+            'width' => 440,
+            'buttons' => array(
+                'Save' => "js:function() {
                     $.ajax({
                         type: 'POST',
                         url: '{$url2}',
@@ -134,13 +136,14 @@ $this->beginWidget('zii.widgets.jui.CJuiDialog', array(
                     });
                     $(this).dialog('close');
                 }",
-            'Cancel' => 'js: function() { $(this).dialog("close"); }'
+                'Cancel' => 'js: function() { $(this).dialog("close"); }'
+            )
         )
-    )
-));
+    ));
 
-echo CHtml::textArea('notes'),
- CHtml::hiddenField('dlgGameId'),
- CHtml::hiddenField('dlgDeckId');
+    echo CHtml::textArea('notes'),
+    CHtml::hiddenField('dlgGameId'),
+    CHtml::hiddenField('dlgDeckId');
 
-$this->endWidget('zii.widgets.jui.CJuiDialog');
+    $this->endWidget('zii.widgets.jui.CJuiDialog');
+}
