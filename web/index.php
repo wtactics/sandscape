48d3942e4f9ecccc29a140ea7cdece2ce1d47de0
Sérgio Lopes <knitter.is@gmail.com>
2016-04-14 12:07:31 +0100

    Adds web/public files, including the entry script.

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+if (is_file(realpath(__DIR__ . '/../vagrant.config.lock'))) {
+
+    defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
+}
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../sandscape/common/config/bootstrap.php');
+
+$config = yii\helpers\ArrayHelper::merge(require(__DIR__ . '/../sandscape/common/config/main.php')
+                , require(__DIR__ . '/../sandscape/management/config/main.php')
+                , require(__DIR__ . '/../sandscape/game/config/main.php'));
+(new yii\web\Application($config))->run();
