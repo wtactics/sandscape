a6f695b296aa7f0601bd140709781d467b1cbc69
Sérgio Lopes <knitter.is@gmail.com>
2016-04-14 16:41:34 +0100

    Fix app configuration.

--- sandscape/management/config/main.php
+++ sandscape/management/config/main.php
@@ -3,28 +3,28 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'sandscape-management',
+    'id' => 'sandscape',
     'basePath' => dirname(__DIR__),
-    'defaultRoute' => 'dashboard',
+    'defaultRoute' => 'sandscape',
     'components' => [
-//TODO: ..
-//        'i18n' => [
-//            'translations' => [
-//                '*' => [
-//                    'class' => 'yii\i18n\PhpMessageSource',
-//                    'basePath' => '@app/messages'
-//                ]
-//            ]
-//        ],
+        //TODO: ..
+        'i18n' => [
+            'translations' => [
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages'
+                ]
+            ]
+        ],
         'urlManager' => ['enablePrettyUrl' => true],
         'request' => [ 'cookieValidationKey' => '0S5_dBjBj92ikshC0BKrVeD0Lo_1S6YFb'],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'loginUrl' => ['dashboard/login']
+            'loginUrl' => ['sandscape/login']
         ],
         'errorHandler' => [
-            'errorAction' => 'dashboard/error'
+            'errorAction' => 'sandscape/error'
         ]
     ],
     'params' => $params
